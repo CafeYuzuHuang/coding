e1e9d3e88640d03191273ebf6d8028ab9c0aa32c
YuzuHuang <72844167+CafeYuzuHuang@users.noreply.github.com>
2021-03-16 16:02:35 +0800

    Update SameTree.py

--- SameTree.py
+++ SameTree.py
@@ -22,6 +22,7 @@ class Solution:
         
         # 2nd solution: iteration
         # https://leetcode.com/problems/same-tree/solution/
+        # About collections.deque: https://desolve.medium.com/%E5%BE%9E%E9%9B%B6%E9%96%8B%E5%A7%8B%E5%AD%B8python-23-588d3d7475d6
         data = deque([(p,q), ])
         while data:
             p, q = data.popleft()
@@ -31,6 +32,20 @@ class Solution:
                 data.append((p.left, q.left))
                 data.append((p.right, q.right))
         return True
+        
+        # 3rd solution: iteration
+        # Use list instead of deque
+        """
+        data = list([(p,q), ])
+        while data:
+            p, q = data.pop(0) # pop the 1st element; i.e. (p, q)
+            if not self.checkBTree(p, q):
+                return False
+            if p:
+                data.append((p.left, q.left))
+                data.append((p.right, q.right))
+        return True
+        """
     
     def checkBTree(self, p, q):
         if p is None and q is None:
@@ -44,6 +59,7 @@ class Solution:
         
         # 1st solution: 28 ms (85%) and 14.2 MB (88%)
         # 2nd solution: 32 ms (62%) and 14.1 MB (88%)
+        # 3rd solution: 28 ms (85%) and 14.4 MB (35%)
         
         # Note:
         # Time complexity: O(N) in worst case.
